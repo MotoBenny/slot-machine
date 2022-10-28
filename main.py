013eaf22f0b475d45249b9eafd30f145210e3645
Benjamin <benj.james.carter@gmail.com>
2022-10-28 14:55:26 -0700

    print slots complete

--- main.py
+++ main.py
@@ -1,8 +1,51 @@
+from calendar import c
+import random
 
 # global constant
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "$": 2,
+    "&": 4,
+    "#": 6,
+    "@": 8
+}
+
+def get_slot_spin(rows, cols, symbols):
+    all_symbols = []
+    # These for loops populate the list. Can we think of a better way to do this without a nested for loop?
+    for symbol, symbol_count in symbols.items(): # items will give me the key and the value for each item in dictionary
+        for _ in range(symbol_count): # _ is anonymous. meaning we wont be needing that variable for anything.
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols): # controls how many columns will be populated with symbols
+        column = []
+        current_symbols = all_symbols[:] # this ensures we are not editing the same list in memory.
+        for _ in range(rows):
+            value = random.choice(all_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    # we need to transpose the matrix here.
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) -1:
+                print(column[row], "|")
+            else:
+                print(column[row])
+
 
 # I feel like there is a better way to confirm the input here. 
 def deposit():
@@ -53,10 +96,19 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
-    total_bet = bet * lines
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You dont have enough money to bet {total_bet}. Your current balance is ${balance}")
+        else:
+            break
+
     print(f"you are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}.")
 
-    print(balance, lines, bet)
+    slots = get_slot_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
+
 
 main()
